cac08ba79eb3937f535dd8f9c0cc089bd787807a
Luke Michalakis <michalakisluke@gmail.com>
2021-09-14 17:41:56 -0400

    <Troubleshoot array verification system>

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,32 @@ $(".search-button").on("click", function (){
     var city = $(".form-input").val();
     console.log(city);
     arrayCheck();
-    console.log(citySaved);
 });
 
 // Check array, clear duplicates
-function arrayCheck() {
-    for (i = 0; i < citySaved.length; i++) {
-        console.log(citySaved[i])
-        if (citySaved.length = 0) {
-            citySaved[i] = city;
-        }
-        else {
-            if ($(citySaved[i].val()) != $(city.val())) {
-                citySaved[i] = city;
-            }
-            else if ($(citySaved[i].val()) === $(city.val())) {
-                console.log("Repeat");
-            }
-        }
+function arrayCheck(citySaved, city) {
+    if (citySaved.indexOf(city) === -1) {
+        citySaved.push(city);
+        console.log(citySaved);
+    }
+    else if (citySaved.indexOf(city) > -1) {
+        console.log("repeat");
     }
 }
 
+// function arrayCheck() {
+//     for (i = 0; i < citySaved.length; i++) {
+//         console.log(citySaved[i])
+//         if (citySaved.length = 0) {
+//             citySaved[i] = city;
+//         }
+//         else {
+//             if ($(citySaved[i].val()) != $(city.val())) {
+//                 citySaved[i] = city;
+//             }
+//             else if ($(citySaved[i].val()) === $(city.val())) {
+//                 console.log("Repeat");
+//             }
+//         }
+//     }
+// }
