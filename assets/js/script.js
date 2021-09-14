6502601647e57b119d451443bc61e201a1fd85b0
Luke Michalakis <michalakisluke@gmail.com>
2021-09-14 19:05:49 -0400

    <Finish array clearing and sorting>

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,29 @@ let citySaved = [];
 
 // Search
 $(".search-button").on("click", function (){
-    var city = $(".form-input").val();
+    city = $(".form-input").val();
     console.log(city);
     arrayCheck();
 });
 
 // Check array, clear duplicates
 function arrayCheck() {
+    console.log("City is " + city);
     console.log(citySaved.indexOf(city));
     if (citySaved.indexOf(city) === -1) {
-        citySaved.push(city);
+        citySaved.unshift(city);
         console.log(citySaved);
     }
     else if (citySaved.indexOf(city) > -1) {
         console.log("repeat");
+        for (i = 0; i < citySaved.length; i++) {
+            if (citySaved[i] === city) {
+                citySaved.splice(i, 1);
+                citySaved.unshift(city);
+                console.log(citySaved);
+            }
+        }
     }
 }
 
-// function arrayCheck() {
-//     for (i = 0; i < citySaved.length; i++) {
-//         console.log(citySaved[i])
-//         if (citySaved.length = 0) {
-//             citySaved[i] = city;
-//         }
-//         else {
-//             if ($(citySaved[i].val()) != $(city.val())) {
-//                 citySaved[i] = city;
-//             }
-//             else if ($(citySaved[i].val()) === $(city.val())) {
-//                 console.log("Repeat");
-//             }
-//         }
-//     }
-// }
+
