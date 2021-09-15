d72b89c2498b6bc4b9967f86ddab48a65c0efc82
Luke Michalakis <michalakisluke@gmail.com>
2021-09-15 14:15:51 -0400

    <Fix list clearing on new submit after refresh>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
 let city;
-let citySaved = [];
+// let citySaved = [];
 
+let citySaved =  JSON.parse(localStorage.getItem("cities"));
+console.log(JSON.parse(localStorage.getItem("cities")));
+
+// Check if data is in local storage
+function onOpen() {
+    if (JSON.parse(localStorage.getItem("cities")) == null) {
+        console.log("It's null");
+        let citySavedLocal = [];
+        window.citySaved = citySavedLocal;
+        console.log(window.citySaved);
+        return citySaved;
+    }
+    else {
+        var citySavedLocal = JSON.parse(localStorage.getItem("cities"));
+        window.citySaved = citySavedLocal;
+        console.log("It's something");
+        return citySaved;
+    }
+}
+
+// Check if data is in local storage, write previous search array
+onOpen();
 previousSearch();
 
-// Search
+// Search button click
 $(".search-button").on("click", function (){
     city = $(".form-input").val().trim();
     console.log(city);
@@ -26,7 +48,9 @@ $(".search-button").on("click", function (){
 
 // Check array, clear duplicates
 function arrayCheck() {
+    citySaved = window.citySaved;
     console.log("City is " + city);
+    console.log(citySaved);
     console.log(citySaved.indexOf(city));
     if (citySaved.indexOf(city) === -1) {
         citySaved.unshift(city);
@@ -47,20 +71,25 @@ function arrayCheck() {
 }
 
 // Write Saved items
-
 function previousSearch() {
-    for (i = 0; i < 10; i++) {
-        var citySavedStore = JSON.parse(localStorage.getItem("cities"));
-        var searchText = citySavedStore[i];
-        console.log(searchText);
-        if (citySavedStore[i] == null) {
-            return;
-        }
-        else {
-            var prevSearchButt = document.createElement("button");
-            prevSearchButt.setAttribute("id", "prev-search-butt")
-            prevSearchButt.innerHTML = citySavedStore[i];
-            document.getElementById("saved").appendChild(prevSearchButt);
+    if (citySaved == null) {
+        return;
+    }
+    else {
+        for (i = 0; i < 10; i++) {
+            // var citySavedStore = JSON.parse(localStorage.getItem("cities"));
+            var citySavedStore = citySaved;
+            var searchText = citySavedStore[i];
+            console.log(searchText);
+            if (citySavedStore[i] == null) {
+                return;
+            }
+            else {
+                var prevSearchButt = document.createElement("button");
+                prevSearchButt.setAttribute("id", "prev-search-butt")
+                prevSearchButt.innerHTML = citySavedStore[i];
+                document.getElementById("saved").appendChild(prevSearchButt);
+            }
         }
     }
 }
